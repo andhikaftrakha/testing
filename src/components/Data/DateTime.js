7ab976cdbb2e1397d07c1aab25d8464e09f5e056
unknown <s3725304@student.rmit.edu.au>
2020-10-17 17:21:55 +1100

    Remove Comment

--- src/components/Data/DateTime.js
+++ src/components/Data/DateTime.js
@@ -1,21 +1,21 @@
-// import { pad } from "../utils/MathUtils";
-// import React, {Component} from 'react'
+import { pad } from "../utils/MathUtils";
+import React, {Component} from 'react'
 
-// export default class DateTime {
-//   getCurrentDateTime() {
-//     const currentdate = new Date();
-//     const currentdatetime =
-//       pad(currentdate.getMonth() + 1, 2) +
-//       "/" +
-//       pad(currentdate.getDate(), 2) +
-//       "/" +
-//       currentdate.getFullYear() +
-//       " " +
-//       pad(currentdate.getHours(), 2) +
-//       ":" +
-//       pad(currentdate.getMinutes(), 2) +
-//       ":" +
-//       pad(currentdate.getSeconds(), 2);
-//     return currentdatetime;
-//   }
-// }
+export default class DateTime {
+  getCurrentDateTime() {
+    const currentdate = new Date();
+    const currentdatetime =
+      pad(currentdate.getMonth() + 1, 2) +
+      "/" +
+      pad(currentdate.getDate(), 2) +
+      "/" +
+      currentdate.getFullYear() +
+      " " +
+      pad(currentdate.getHours(), 2) +
+      ":" +
+      pad(currentdate.getMinutes(), 2) +
+      ":" +
+      pad(currentdate.getSeconds(), 2);
+    return currentdatetime;
+  }
+}
